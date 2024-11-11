74e03247a70b35a48ace88e737b6995ebd86e08f
mrebecalg <mrebecalg@gmail.com>
2024-11-11 17:06:06 -0400

    Algunas traducciones realizadas, cambios en Publicaciones

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,3 +1,6 @@
 class Publication < ApplicationRecord
   mount_uploader :picture, PictureUploader
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Custom password complexity validation
+  validate :password_complexity
+
+  private
+  
+  def password_complexity
+     if password.present?
+      unless password =~ /\d/ && password =~ /[^A-Za-z0-9]/
+        errors.add :La_contraseña, "debe incluir al menos un número y un carácter especial"
+      end
+    end
+  end
 end
