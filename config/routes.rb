5f0f12b767e65ae1e5a760b1fd582219b6af4e76
mrebecalg <mrebecalg@gmail.com>
2024-11-13 16:43:14 -0400

    Cambios en el inicio

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,16 @@ Rails.application.routes.draw do
   resources :requests
   devise_for :users, path: 'users'
 
-  get "pages/profile"
+
   resources :publications, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resource :profile, only: [:show, :edit, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "pages#homepage"
+  root "publications#index"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get 'pages/profile', to: 'pages#profile'
+
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
